fc8f518abf07fe009a669ccb3bebe8cd32141b83
Krishan König <hello@krishankoenig.com>
2026-03-28 10:55:03 +0100

    chore: bump hearth to v0.2.3

--- Formula/hearth.rb
+++ Formula/hearth.rb
@@ -1,16 +1,16 @@
 class Hearth < Formula
   desc "Unified Laravel development command center"
   homepage "https://github.com/Naoray/hearth"
-  version "0.2.1"
+  version "0.2.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Naoray/hearth/releases/download/v0.2.1/hearth-v0.2.1-aarch64-apple-darwin.tar.gz"
-      sha256 "52fd146672720737bc26667c093268baac1c55519a78c9e74539eddff35f7dc4"
+      url "https://github.com/Naoray/hearth/releases/download/v0.2.3/hearth-v0.2.3-aarch64-apple-darwin.tar.gz"
+      sha256 "c83b4ea5461c4cd14ec79ed4b6b647632b517f8bfa9127b77d5c2af0738ac964"
     else
-      url "https://github.com/Naoray/hearth/releases/download/v0.2.1/hearth-v0.2.1-x86_64-apple-darwin.tar.gz"
-      sha256 "23878c26b5d32e53ab2232be19c991d93b50f290639516e11d234e23f5c96fc1"
+      url "https://github.com/Naoray/hearth/releases/download/v0.2.3/hearth-v0.2.3-x86_64-apple-darwin.tar.gz"
+      sha256 "dac3618ad23588a2b689ebba63bbf4531b99f469e2d7ceeb7982217681ce46e6"
     end
   end
 
